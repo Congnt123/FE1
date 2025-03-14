eaa56de1ebcd7d155d9f4eb19bee7d9f2e65d07d
Congnt123 <nguyenthanhcong24112018@gmai.com>
2025-03-14 23:24:48 +0700

    update

--- src/app/pages/ui-components/category/category.component.ts
+++ src/app/pages/ui-components/category/category.component.ts
@@ -18,8 +18,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'cong lox', weight: 20.1797, symbol: 'cong khung'},
-
 ];
 
 @Component({
